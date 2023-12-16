908dac31ec1e22bd988269176fc473411a1fb55b
Victorinox2 <uwaomaviktor@yahoo.com>
2023-12-16 13:18:17 -0800

    config management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,16 @@
+# create a file in the /tmp
+
+$dir_path = '/tmp'
+$file_path = '/tmp/school'
+
+file { $dir_path:
+  ensure => 'directory',
+}
+
+file { $file_path:
+  ensure  => 'file',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
